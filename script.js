0cdec766b1448bdde5ad33aa0bd8250d4b2d1cf2
Bexruz <behruzbakhodirhodjaev12@gmail.com>
2025-01-17 20:17:52 +0500

    first commit .git

---
+++ script.js
@@ -0,0 +1,17 @@
+let color = 'blue';
+
+switch (color){
+    case 'red':
+        alert("Red")
+        brake;
+    case 'blue':
+        alert("blue")
+        brake;
+    case 'blue':
+        alert("blue")
+        brake;
+    case 'black':
+        alert("black")
+        brake;
+}
+
